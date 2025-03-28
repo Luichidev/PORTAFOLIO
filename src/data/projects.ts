8f4483e72a5e672b8265738c3a8d0554958ec696
Luis <luichidev@gmail.com>
2025-03-28 17:36:19 +0100

    Añadir las card de los proyectos y sus datos

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+	{
+		title: 'Multiestetica',
+		description: 'Desarrollo y depuración',
+		image: 'multiestetica.jpg',
+		link: 'https://www.multiestetica.com/'
+	},
+	{
+		title: 'Pizzeria NamNam',
+		description: 'Diseño web',
+		image: 'pizzeria.jpg',
+		link: 'https://www.xn--pizzeriafamiliaramam-j7bc.com/pizzas/'
+	},
+	{
+		title: 'Barco de Alquiler',
+		description: 'Desarrollo y depuración',
+		image: 'barco_alquiler.jpg',
+		link: 'https://www.barcodealquiler.com/es/'
+	},
+	{
+		title: 'Simona',
+		description: 'Desarrollo y depuración',
+		image: 'simona.jpg',
+		link: '#'
+	},
+	{
+		title: 'Metro-Electonica',
+		description: 'Desarrollo del CRM',
+		image: 'metro.jpg',
+		link: 'https://www.metroelectronica.com/'
+	},
+	{
+		title: 'Yacht Services',
+		description: 'Desarrollo e integración',
+		image: 'yates.jpg',
+		link: 'https://www.onboardyachtservices.com/es/'
+	},
+	{
+		title: 'La Marea',
+		description: 'Añadir nuevas funcionalidades',
+		image: 'lamarea.webp',
+		link: 'https://www.lamarea.com/'
+	}
+]
