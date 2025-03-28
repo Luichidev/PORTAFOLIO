a90238145324d9e4e48f33675fd32f8d42703c7e
Luis <luichidev@gmail.com>
2025-03-28 17:31:27 +0100

    Añadir tailwind y robottxt a la configuración de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), robotsTxt()]
+  }
+});
