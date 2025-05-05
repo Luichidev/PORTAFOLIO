eff9423eacd21ccec127a8a278de13a30d383e61
Luis <luichidev@gmail.com>
2025-05-05 20:12:29 +0200

    Agregar propiedad 'current' a las experiencias y corregir formato de descripción

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -3,24 +3,29 @@ export const experiences = [
 		company: 'Lite Solutions',
 		role: 'Programador web - Q&A engineer',
 		period: '2022 → Actualmente',
-		description: 'Automatización (BDD, e2e) de un software médico. Migración del proyecto Multiestetica.com de symfony a VUE 3. Añadir nuevas funcionalidades al Wordpress de la revista la Marea.com.'
+		description: 'Automatización (BDD, e2e) de un software médico. Migración del proyecto Multiestetica.com de symfony a VUE 3. Añadir nuevas funcionalidades al Wordpress de la revista la Marea.com.',
+		link: 'https://www.litesolutions.es/',
+		current: true
 	},
 	{
 		company: 'Idearium3.0',
 		role: 'Programador web',
 		period: '2021 → 2021',
-		description: 'Migración de páginas de la empresa a WORDPRESS. Análisis de necesidades de programas (Bug) de WORDPRESS. Automatización de tareas requeridas por parte del SEO de la empresa. Creación de Scripts para la automatización de tareas comunes de la empresa.'
+		description: 'Migración de páginas de la empresa a WORDPRESS. Análisis de necesidades de programas (Bug) de WORDPRESS. Automatización de tareas requeridas por parte del SEO de la empresa. Creación de Scripts para la automatización de tareas comunes de la empresa.',
+		current: false
 	},
 	{
 		company: 'Land Networking',
 		role: 'Programador web',
 		period: '2018 → 2020',
-		description: 'Añadir o corregir nuevas funcionalidades de los diferentes proyecto (CRM). Diseño Frontend de los nuevos proyectos. Buscar y solucionar errores en proyectos antiguos. Tecnologías usadas JS,JERY, PHP, JAVASCRIPT, HTML2PDF, CSS, HTML.'
+		description: 'Añadir o corregir nuevas funcionalidades de los diferentes proyecto (CRM). Diseño Frontend de los nuevos proyectos. Buscar y solucionar errores en proyectos antiguos. Tecnologías usadas JS,JERY, PHP, JAVASCRIPT, HTML2PDF, CSS, HTML.',
+		current: false
 	},
 	{
 		company: 'Escuela de la memoria',
 		role: 'Programador web',
 		period: '2017 → 2017',
-		description: 'Añadir nuevas funcionalidades a todo los juegos de pago de la empresa. Modificar, Cambiar o Crear nuevos juegos para desarrollar la memoria. Controlar el acceso a usuarios en modo gratuito y de pago. Tecnologías usadas PHP, JQUERY, AJAX, XAMMP, HTML, CSS, JAVASCRIPT.'
+		description: 'Añadir nuevas funcionalidades a todo los juegos de pago de la empresa. Modificar, Cambiar o Crear nuevos juegos para desarrollar la memoria. Controlar el acceso a usuarios en modo gratuito y de pago. Tecnologías usadas PHP, JQUERY, AJAX, XAMMP, HTML, CSS, JAVASCRIPT.',
+		current: false
 	}
 ]
