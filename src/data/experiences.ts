3b1994d37b6b2352de396df2de3ea6f344ca1c68
Luis <luichidev@gmail.com>
2025-11-04 00:07:06 +0100

    Actualizar roles y enlaces en experiencias, eliminando el período y corrigiendo descripciones

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,31 +1,36 @@
 export const experiences = [
 	{
-		company: 'Lite Solutions',
-		role: 'Programador web - Q&A engineer',
-		period: '2022 → Actualmente',
-		description: 'Automatización (BDD, e2e) de un software médico. Migración del proyecto Multiestetica.com de symfony a VUE 3. Añadir nuevas funcionalidades al Wordpress de la revista la Marea.com.',
-		link: 'https://www.litesolutions.es/',
+		company: 'Roche Diagnostics',
+		role: 'Q&A engineer (Contractor)',
+		description: 'Automatización (BDD, e2e) de un software médico. Desarrollo de pruebas manuales y automatizadas. Análisis de requisitos y documentación de pruebas. Herramientas: Jira, Swagger, Selenium, Specflow, C#, GIT, Retina Codebeamer.',
+		link: 'https://www.roche.es/',
 		current: true
 	},
 	{
+		company: 'Lite Solutions',
+		role: 'FrontEnd Engineer',
+		description: 'Migración del proyecto Multiestetica.com de symfony a VUE 3. Añadir nuevas funcionalidades al Wordpress de la revista la Marea.com.',
+		link: 'https://www.litesolutions.es/',
+		current: false
+	},
+	{
 		company: 'Idearium3.0',
-		role: 'Programador web',
-		period: '2021 → 2021',
+		role: 'FrontEnd Engineer',
 		description: 'Migración de páginas de la empresa a WORDPRESS. Análisis de necesidades de programas (Bug) de WORDPRESS. Automatización de tareas requeridas por parte del SEO de la empresa. Creación de Scripts para la automatización de tareas comunes de la empresa.',
+		link: 'https://idearium.es/',
 		current: false
 	},
 	{
 		company: 'Land Networking',
-		role: 'Programador web',
-		period: '2018 → 2020',
+		role: 'FrontEnd Engineer',
 		description: 'Añadir o corregir nuevas funcionalidades de los diferentes proyecto (CRM). Diseño Frontend de los nuevos proyectos. Buscar y solucionar errores en proyectos antiguos. Tecnologías usadas JS,JERY, PHP, JAVASCRIPT, HTML2PDF, CSS, HTML.',
 		current: false
 	},
 	{
 		company: 'Escuela de la memoria',
-		role: 'Programador web',
-		period: '2017 → 2017',
+		role: 'FrontEnd Engineer',
 		description: 'Añadir nuevas funcionalidades a todo los juegos de pago de la empresa. Modificar, Cambiar o Crear nuevos juegos para desarrollar la memoria. Controlar el acceso a usuarios en modo gratuito y de pago. Tecnologías usadas PHP, JQUERY, AJAX, XAMMP, HTML, CSS, JAVASCRIPT.',
+		link: 'https://escueladelamemoria.com/',
 		current: false
 	}
 ]
